79719ccab831c0c1c8e5c2bbe757306f75c3e5ca
Nookriy <a.azman@student.saedubai.com>
2019-10-29 15:20:17 +0400

    Movement + Firing

---
+++ Assets/Scripts/DestroyBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SelfDestruct", 3f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //damage or destroy enemy
+            //add score MAYBE
+            //play audio
+            //
+            SelfDestruct();
+        }
+    }
+
+    public void SelfDestruct()
+        {
+            Destroy(gameObject);
+        }
+    }
+
---
+++ Assets/Scripts/PlaneMov.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMov : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    float force;
+    [SerializeField]
+    float RotationSpeed;
+    private GameObject bulletplaceHolder;
+    [SerializeField]
+    GameObject bulletPrefab;
+    [SerializeField]
+    float bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        bulletplaceHolder = GameObject.FindGameObjectWithTag("BPlaceHolder");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Controls();
+        Shooting();
+
+    }
+
+    void Controls()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rb.AddForce(transform.up * y * force);
+
+        transform.Rotate(new Vector3(0,0,RotationSpeed * -x), Space.Self);
+
+    }
+
+    void Shooting()
+    {
+        GameObject b;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            b = Instantiate(bulletPrefab, bulletplaceHolder.transform.position, Quaternion.identity);
+            b.GetComponent<Rigidbody>().AddForce(transform.up * bulletSpeed);
+        }
+        
+    }
+}
