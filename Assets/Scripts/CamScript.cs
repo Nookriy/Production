30f86ad0b9edb41c70c45e1c3aa10c53e2214d27
Nookriy <a.azman@student.saedubai.com>
2019-11-13 22:25:10 +0400

    - Camera Movement/Placement - Invisible Borders

--- Assets/Scripts/CamScript.cs
+++ Assets/Scripts/CamScript.cs
@@ -6,6 +6,9 @@ public class CamScript : MonoBehaviour
 {
     private PlaneMov player;
 
+    public float minDistx,maxDistx,minDisty, maxDisty, camSpeed;
+    
+
     public Vector3 offset;
 
     private Camera mainCam;
@@ -15,11 +18,28 @@ public class CamScript : MonoBehaviour
         player = GameObject.FindObjectOfType<PlaneMov>();
         mainCam = Camera.main;
         mainCam.transform.position = player.transform.position + offset;
+        //Debug.Log(Vector3.Distance(gameObject.transform.position, player.gameObject.transform.position));
+        //Debug.Log((transform.position.x - player.transform.position.x) + " , " + (transform.position.y - player.transform.position.y));
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        mainCam.transform.position = player.transform.position + offset;
+        CamFollow();
+        
+    }
+
+    private void CamFollow()
+    {
+        //y dist between -9 and 9 camera will not follow OR x dist between 20 and -20 camm wil not follow
+        //mainCam.transform.position = player.transform.position + offset;
+        if (transform.position.x - player.transform.position.x > maxDistx || transform.position.x - player.transform.position.x < minDistx ||
+           transform.position.y - player.transform.position.y > maxDisty || transform.position.y - player.transform.position.y < minDisty)
+        {
+            mainCam.transform.position = Vector3.MoveTowards(transform.position, player.transform.position + offset, camSpeed);
+        }
+        //Debug.Log(Vector3.Distance(gameObject.transform.position, player.gameObject.transform.position));
+        //Debug.Log((transform.position.x - player.transform.position.x) + " , " + (transform.position.y - player.transform.position.y));
     }
 }
+
